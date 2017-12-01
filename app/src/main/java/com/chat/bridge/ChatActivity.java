57348a43d03ea1db2419d775c18ff8629bd5ba92
Shantanu Pramanik <srs.shaan@gmail.com>
2017-12-01 23:42:54 +0530

    Sending messages and storing them to the database

--- app/src/main/java/com/chat/bridge/ChatActivity.java
+++ app/src/main/java/com/chat/bridge/ChatActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,12 +25,15 @@ import com.squareup.picasso.Picasso;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 public class ChatActivity extends AppCompatActivity {
 
     private static final String TAG = "ChatActivity";
     private String chatUserId, userName;
+    private String currentUserId;
     private DatabaseReference rootRef;
     private DatabaseReference currentUserRef;
 
@@ -36,6 +42,10 @@ public class ChatActivity extends AppCompatActivity {
     private CircularImageView profileImage;
     private FirebaseAuth mAuth;
 
+    private ImageButton ibChatAdd;
+    private ImageButton ibChatSend;
+    private EditText etMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +53,17 @@ public class ChatActivity extends AppCompatActivity {
 
         chatUserId = getIntent().getStringExtra("userId");
         userName = getIntent().getStringExtra("userName");
+
+        ibChatAdd = (ImageButton) findViewById(R.id.ibChatAdd);
+        ibChatSend = (ImageButton) findViewById(R.id.ibChatSend);
+        etMessage = (EditText) findViewById(R.id.etMessage);
+
+        ibChatSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -60,6 +81,7 @@ public class ChatActivity extends AppCompatActivity {
 
         tvDisplayName.setText(userName);
         mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
         currentUserRef = FirebaseDatabase.getInstance().getReference().child("Users")
                 .child(mAuth.getCurrentUser().getUid());
 
@@ -85,6 +107,67 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
         actionBar.setCustomView(actionBarView);
+
+        rootRef.child("Chat").child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChild(chatUserId)) {
+                    Map chatAddMap = new HashMap();
+                    chatAddMap.put("seen", "false");
+                    chatAddMap.put("timestamp", ServerValue.TIMESTAMP);
+
+                    Map chatUserMap = new HashMap();
+                    chatUserMap.put("Chat/" + currentUserId + "/" + chatUserId, chatAddMap);
+                    chatUserMap.put("Chat/" + chatUserId + "/" + currentUserId, chatAddMap);
+
+                    rootRef.updateChildren(chatUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                            if (databaseError != null) {
+                                Log.e(TAG, "onComplete: " + databaseError.getMessage());
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendMessage() {
+        String message = etMessage.getText().toString().trim();
+        etMessage.setText("");
+        if (!TextUtils.isEmpty(message)) {
+
+            DatabaseReference userMessageRef = rootRef.child("Messages")
+                    .child(currentUserId).child(chatUserId).push();
+            String pushId = userMessageRef.getKey();
+            String currentUserRef = "Messages/" + currentUserId + "/" + chatUserId;
+            String chatUserRef = "Messages/" + chatUserId + "/" + currentUserId;
+
+            Map messageMap = new HashMap();
+            messageMap.put("message", message);
+            messageMap.put("seen", "false");
+            messageMap.put("time", ServerValue.TIMESTAMP);
+            messageMap.put("type", "text");
+
+            Map messageUserMap = new HashMap();
+            messageUserMap.put(currentUserRef + "/" + pushId, messageMap);
+            messageUserMap.put(chatUserRef + "/" + pushId, messageMap);
+
+            rootRef.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                    if (databaseError != null) {
+                        Log.e(TAG, "onComplete: " + databaseError.getMessage());
+                    }
+                }
+            });
+        }
     }
 
     @Override
